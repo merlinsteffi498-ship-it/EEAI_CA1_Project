8bbab9282d4bf2921d74feca6554defab1346669
Merlin <merlinsteffi498@gmail.com>
2026-03-23 23:22:52 +0000

    feat: implement train_test_split in data_model.py

--- modelling/data_model.py
+++ modelling/data_model.py
@@ -9,11 +9,24 @@ random.seed(seed)
 np.random.seed(seed)
 
 class Data():
-    def __init__(self,
-                 X: np.ndarray,
-                 df: pd.DataFrame) -> None:
-                 # This method will create the model for data
-                 #This will be performed in second activity
+    def __init__(self, X: np.ndarray, df: pd.DataFrame, target_col: str = Config.CLASS_COL) -> None:
+        """
+        Encapsulates the data and performs the train-test split.
+        """
+        # 1. Store the full embeddings and target variable
+        self.embeddings = X
+        self.y = df[target_col].values
+
+        # 2. Split the data into 80% training and 20% testing
+        # We also split the indices to keep track of the original dataframe rows
+        indices = np.arange(len(df))
+        self.X_train, self.X_test, self.y_train, self.y_test, idx_train, idx_test = train_test_split(
+            self.embeddings, self.y, indices, test_size=0.2, random_state=seed
+        )
+        
+        # 3. Store the split dataframes
+        self.train_df = df.iloc[idx_train]
+        self.test_df = df.iloc[idx_test]
 
 
     def get_type(self):
